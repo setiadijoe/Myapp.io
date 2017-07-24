3bb89c214db1f1fc0357710bb27174ab62781517
setiadijoe <setiadi_y56@yahoo.co.id>
2017-07-24 23:46:05 +0700

    Update Script
    Script baru

--- script.js
+++ script.js
@@ -27,7 +27,7 @@
 	// function to return correct answer string
 		function correctAnswer (correctStringNo, qNumber) {
 			console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
-			return ("Jawaban yang benar untuk nomor #" + qNumber + ": &nbsp;<strong>" +
+			return ("Jawaban yang benar #" + qNumber + ": &nbsp;<strong>" +
 				(document.getElementById(correctStringNo).innerHTML) + "</strong>");
 			}
 
@@ -57,11 +57,14 @@
 		var showScore = "Your Score: " + calcScore +"/" + questionCounter;
 	// if 4/4, "perfect score!"
 		if (calcScore === questionCounter) {
-			showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
-			alert ("KAMU SUKSES");
+			showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>";
+			alert ('Kamu berhasil!');
 		}else{
-			alert ("KAMU GAGAL. Makan dulu sanah!!");
+			alert ('Kamu gagal, makan dulu sana!')
+		}
+		;
 		document.getElementById('userScore').innerHTML = showScore;
+
 	}
 
 $(document).ready(function() {
